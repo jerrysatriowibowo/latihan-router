be0d42d60cbbdb6d33f4e6263c2d34addeea0978
Risman04 <risman4205@gmail.com>
2023-07-31 12:32:23 +0700

    Latihan Router

---
+++ src/assets/Produk.js
@@ -0,0 +1,34 @@
+export const produk = {
+    "produk" : [
+        {
+            "id": 1,
+            "nama": "Handphone",
+            "harga": "Rp 1.000.000",
+        },
+        {
+            "id": 2,
+            "nama": "Laptop",
+            "harga": "Rp 5.000.000",
+        },
+        {
+            "id": 3,
+            "nama": "Televisi",
+            "harga": "Rp 3.000.000",
+        },
+        {
+            "id": 4,
+            "nama": "Mobil",
+            "harga": "Rp 110.000.000",
+        },
+        {
+            "id": 5,
+            "nama": "Motor",
+            "harga": "Rp 17.000.000",
+        },
+        {
+            "id": 6,
+            "nama": "Sepatu",
+            "harga": "Rp 250.000",
+        },
+    ]
+}
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>About Page</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+    setup(props, context)
+    {
+        onMounted(() => {
+            context.emit("id-menu", 3)
+        })
+    }
+}
+</script>
